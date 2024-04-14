3ad034654222d3038e3714c9129d1f9e828d987d
Shey Yi <shey@email.com>
2024-04-14 20:55:59 -0300

    terceiro 1:37:35

--- ProjetoPyGame/Codes/Const.py
+++ ProjetoPyGame/Codes/Const.py
@@ -1,2 +1,13 @@
+# C
+COLOR_RED = (220, 20, 60)
+COLOR_YELLOW = (255, 255, 128)
+COLOR_WHITE= (245, 245, 245)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'EXIT')
+# W
 WIN_WIDTH = 600
 WIN_HEIGHT = 337
--- ProjetoPyGame/Codes/Menu.py
+++ ProjetoPyGame/Codes/Menu.py
@@ -1,8 +1,10 @@
+import sys
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from Codes.Const import WIN_WIDTH
+from Codes.Const import WIN_WIDTH, COLOR_RED, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -14,15 +16,27 @@ class Menu:
     def run(self):
         pygame.mixer_music.load('./asset/MenuMusic.mp3')
         pygame.mixer_music.play(-1)
+        menu_option = 0
         while True:
             self.screen_Surface.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Project", (213, 59, 175), ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "PyGame", (213, 59, 175), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Project", COLOR_RED, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "PyGame", COLOR_RED, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, (WIN_WIDTH / 2, 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, (WIN_WIDTH / 2, 200 + 30 * i))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
 
             pygame.display.flip()
 
-    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_por: tuple):
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color)
-        text_rect: Rect = text_surf.get_rect(center=text_center_por)
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.screen_Surface.blit(source=text_surf, dest=text_rect)
