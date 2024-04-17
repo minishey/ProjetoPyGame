fd25b5d3bd17bb9afa535842886108ae7112095b
Shey Yi <32911507+minishey@users.noreply.github.com>
2024-04-17 12:28:40 -0300

    Update Background.py

--- ProjetoPyGame/Codes/Background.py
+++ ProjetoPyGame/Codes/Background.py
@@ -4,9 +4,11 @@
 from Entity import Entity
 
 class Background(Entity):
-    def __init__(self):
+    
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
         pass
 
-    def move(self, ):
+    def move(self):
         pass
 
